fec5279bf8a7603a82bed69a92f73149d9a639bd
SummerNightRunner <hackermanspacex@gmail.com>
2026-05-30 14:37:48 +0300

    Split practice handlers

--- src/bot/handlers/__init__.py
+++ src/bot/handlers/__init__.py
@@ -1,9 +1,11 @@
 from aiogram import Dispatcher
 
 from bot.handlers.course import router as course_router
+from bot.handlers.practice import router as practice_router
 from bot.handlers.start import router as start_router
 
 
 def register_routers(dispatcher: Dispatcher) -> None:
     dispatcher.include_router(course_router)
+    dispatcher.include_router(practice_router)
     dispatcher.include_router(start_router)
